2959df644dc4273c2901333e76f41f23aa6dcac0
c3ll4rd00r <giacomo.bndt@gmail.com>
2020-02-10 12:51:39 +0100

    Settings activity created

---
+++ app/src/main/java/com/example/velocok_beta/SettingsActivity.java
@@ -0,0 +1,45 @@
+package com.example.velocok_beta;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.PopupWindow;
+import android.widget.TextView;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceFragmentCompat;
+
+import org.w3c.dom.Text;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    PopupWindow popUp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.settings, new SettingsFragment())
+                .commit();
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    public static class SettingsFragment extends PreferenceFragmentCompat {
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey);
+        }
+    }
+
+    public void darkModeSetting(View v) {
+        popUp = new PopupWindow(this);
+        popUp.showAtLocation(v, Gravity.BOTTOM, 10,10);
+        popUp.update(50,50,300,80);
+    }
+}
