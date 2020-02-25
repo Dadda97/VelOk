f56d3ff74c67e8cd052238e89f0a4e6842b99e3b
Dadda97 <Dadda97@users.noreply.github.com>
2020-02-25 20:33:25 +0100

    Finished with news in welcomeActivity

---
+++ app/src/main/java/com/example/velocok_beta/MyNewsProvider.java
@@ -0,0 +1,93 @@
+package com.example.velocok_beta;
+
+import android.media.Image;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MyNewsProvider {
+    private static  final String TAG = "MyNewsProvider";
+    private static  final String STARTURL="https://newsapi.org/v2/everything?language=it&sortby=relevancy&domains=ansa.it&from=";
+    private int numberOfNews;
+    private String[] titles =  null;
+    private String[] bodies = null;
+    private String[] images = null;
+
+    public MyNewsProvider(int num){
+        numberOfNews = num;
+        titles = new String[numberOfNews];
+        images = new String[numberOfNews];
+        bodies = new String[numberOfNews];
+        getNewsFromApi();
+
+    }
+
+    public String getNewsTitle(int i){
+          return titles[i];
+    }
+
+    public String getNewsImagesURL(int i){
+        return images[i];
+    }
+
+    public String getNewsBody(int i){
+        return bodies[i];
+    }
+
+    private String buildURL(){
+        SimpleDateFormat formatter = new SimpleDateFormat("MM-dd-yyyy");
+        Date date = new Date();
+        String dateString=formatter.format(date);
+        String apiString= "&apiKey=".concat(BuildConfig.NEWS_KEY);
+        return STARTURL.concat(dateString).concat(apiString);
+    }
+    private void getNewsFromApi() {
+        Log.d(TAG,buildURL());
+        try {
+            InputStream is = new URL(buildURL()).openStream();
+            try {
+
+                BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+                String jsonText = readAll(rd);
+                JSONObject json = new JSONObject(jsonText);
+                JSONArray JsonArticles =(json.getJSONArray("articles"));
+                Log.d(TAG,JsonArticles.toString());
+
+                for (int i = 0; i < numberOfNews; i++) {
+                    titles[i] = ( (JSONObject)   JsonArticles.get(i)).getString("title");
+                    images[i] = ( (JSONObject)   JsonArticles.get(i)).getString("urlToImage");
+                    bodies[i] = ( (JSONObject)   JsonArticles.get(i)).getString("content");
+                }
+
+            } finally {
+                is.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String readAll(Reader rd) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int cp;
+        while ((cp = rd.read()) != -1) {
+            sb.append((char) cp);
+        }
+        return sb.toString();
+    }
+}
---
+++ app/src/main/java/com/example/velocok_beta/WelcomeActivity.java
@@ -0,0 +1,122 @@
+package com.example.velocok_beta;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.example.velocok_beta.MyNewsProvider;
+import com.squareup.picasso.Picasso;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.net.URL;
+
+public class WelcomeActivity extends AppCompatActivity {
+    private final String TAG="Welcome Activity";
+    private LinearLayout newsParent;
+    private int numberOfNews;
+    MyNewsProvider newsProvider;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        newsParent = findViewById(R.id.newsParent);
+        numberOfNews =  newsParent.getChildCount();     //number of news defined by XML
+        Log.d(TAG,String.valueOf(numberOfNews));
+
+        newsProvider = new MyNewsProvider(numberOfNews);
+
+        Picasso.get().setLoggingEnabled(true);
+
+        for (int i = 0; i< numberOfNews;i++){
+
+            LinearLayout news= (LinearLayout) newsParent.getChildAt(i);
+
+            TextView text= (TextView) news.getChildAt(0);
+            text.setText(newsProvider.getNewsTitle(i));
+
+            ImageView image= (ImageView) news.getChildAt(1);
+            image.setContentDescription(newsProvider.getNewsTitle(i));
+            Picasso.get().load(newsProvider.getNewsImagesURL(i)).resize(500, 500)
+              .centerCrop().into(image);
+        }
+
+    }
+
+   public void openNewsBody(View v){
+       AlertDialog.Builder ad = new AlertDialog.Builder(this);
+       int newsId = Integer.parseInt((String)v.getTag());
+       ad.setTitle(newsProvider.getNewsTitle(newsId));
+       ad.setMessage(newsProvider.getNewsBody(newsId));
+       ad.show();
+   }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //TODO add execute of mycore
+
+                } else {
+                    Toast toast = Toast.makeText(this, "The app cannot work without GPS permission", Toast.LENGTH_LONG);
+
+                }
+                return;
+            }
+            case 2: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //TODO add execute of mycore
+
+                } else {
+                    Toast toast = Toast.makeText(this, "Without Internet permission the information shown in the app will be reduced", Toast.LENGTH_LONG);
+
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
+    }
+
+    private void checkPermission(){
+        if (ContextCompat.checkSelfPermission( this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Log.e(TAG,"no GPS permissions");
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION},1
+            );
+
+            return;
+        }
+        if (ContextCompat.checkSelfPermission( this, Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_NETWORK_STATE) != PackageManager.PERMISSION_GRANTED) {
+            Log.e(TAG,"no INTERNET permissions");
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.INTERNET,Manifest.permission.ACCESS_NETWORK_STATE},2
+            );
+
+            return;
+        }
+    }
+}
